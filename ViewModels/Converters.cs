476caabac0052fd627072a0fa8dda2be0449b4a9
idea Good <3112984410@qq.com>
2023-09-27 16:19:04 +0800

    Basic archive extraction test.

--- ViewModels/Converters.cs
+++ ViewModels/Converters.cs
@@ -21,9 +21,50 @@ namespace _7zip.ViewModels
                 return null;
             }
         }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotSupportedException();
+        }
+    }
+
+    public class BooleanInversionConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is bool b)
+                return !b;
+            return false;
+        }
+
         public object ConvertBack(object value, Type targetType, object parameter, string language)
         {
             throw new NotImplementedException();
+        }
+    }
+
+    public class BasicCalculateConverter : IValueConverter
+    {
+        public virtual object Convert(object value, Type targetType, object parameter, string language)
+        {
+            string para = parameter as string;
+            char op = para[0];
+            double rightVal = double.Parse(para[1..]);
+            double leftVal = System.Convert.ToDouble(value);
+            return op switch
+            {
+                '+' => leftVal + rightVal,
+                '-' => leftVal - rightVal,
+                '*' => leftVal * rightVal,
+                '/' => leftVal / rightVal,
+                '%' => leftVal % rightVal,
+                _ => throw new InvalidDataException($"{nameof(BasicCalculateConverter)}: Invalid Number Or Operator.")
+            };
+        }
+
+        public virtual object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return Convert(value, targetType, parameter, language);
         }
     }
 }
--- ViewModels/ExtractionViewModel.cs
+++ ViewModels/ExtractionViewModel.cs
@@ -4,6 +4,7 @@ using SevenZip;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,15 +13,20 @@ namespace _7zip.ViewModels
 {
     internal partial class ExtractionViewModel : ObservableObject
     {
+        #region Private Fields
         SevenZipExtractor extractor;
+        bool isExtractingWholeArchive; //是否正在直接导出整个压缩包
+        bool shouldCancelWork = false;
+        #endregion
 
+        #region MVVM Properties
         /// <summary>
         /// 获取压缩文件的路径。
         /// </summary>
         public string ArchivePath => extractor.FileName;
 
         /// <summary>
-        /// 要导出的文件的索引集合。若为空，则应解压所有文件。
+        /// 要导出的文件的索引集合。若为空，则解压所有文件。该集合不应有重复元素。
         /// </summary>
         [ObservableProperty]
         ObservableCollection<int> fileIndexes = new();
@@ -32,23 +38,41 @@ namespace _7zip.ViewModels
         string outputDirPath;
 
         /// <summary>
-        /// 获取当前解压操作的总进度(0f-1f)。
+        /// 获取或设置当前解压操作的总进度(0f-1f)。
         /// </summary>
         [ObservableProperty]
         float extractPercentage;
 
         /// <summary>
-        /// 获取当前正在解压的文件名。
+        /// 获取或设置当前正在解压的文件名。
         /// </summary>
         [ObservableProperty]
         string currentExtractingFileName;
 
         /// <summary>
-        /// 获取当前正在解压的文件索引。
+        /// 获取或设置当前正在解压的文件索引。
         /// </summary>
         [ObservableProperty]
         int currentExtractingIndex;
 
+        /// <summary>
+        /// 获取或设置一个值，指示了解压操作是否完成。
+        /// </summary>
+        [ObservableProperty]
+        bool isExtractionFinished = false;
+
+        /// <summary>
+        /// 获取或设置需要导出的文件总个数。
+        /// </summary>
+        [ObservableProperty]
+        int totalFilesCount = -1;
+
+        /// <summary>
+        /// 获取或设置已导出的文件个数。
+        /// </summary>
+        [ObservableProperty]
+        int extractedFilesCount = 0;
+        #endregion
         public ExtractionViewModel(string archivePath)
         {
             extractor = new SevenZipExtractor(archivePath)
@@ -61,20 +85,35 @@ namespace _7zip.ViewModels
         void EventStartup()
         {
             extractor.Extracting += Extractor_Extracting;
+            extractor.ExtractionFinished += Extractor_ExtractionFinished;
             extractor.FileExtractionStarted += Extractor_FileExtractionStarted;
             extractor.FileExtractionFinished += Extractor_FileExtractionFinished;
             this.PropertyChanged += ExtractViewModel_PropertyChanged;
         }
 
+        private void Extractor_ExtractionFinished(object sender, EventArgs e)
+        {
+            
+        }
+
         private void Extractor_FileExtractionStarted(object sender, FileInfoEventArgs e)
         {
+            if (isExtractingWholeArchive) TotalFilesCount = (int)extractor.FilesCount;
+
+            e.Cancel = shouldCancelWork;
+
             CurrentExtractingFileName = e.FileInfo.FileName;
             CurrentExtractingIndex = e.FileInfo.Index;
         }
 
         private void Extractor_FileExtractionFinished(object sender, FileInfoEventArgs e)
         {
-            
+            ExtractedFilesCount++;
+
+            e.Cancel = shouldCancelWork;
+
+            if (ExtractedFilesCount == TotalFilesCount)
+                IsExtractionFinished = true;
         }
 
         private void Extractor_Extracting(object sender, ProgressEventArgs e)
@@ -86,15 +125,36 @@ namespace _7zip.ViewModels
         {
         }
 
+        /// <summary>
+        /// 重置解压操作的所有统计数据，以便执行新的解压操作。
+        /// </summary>
+        void ResetExtractionStatistics()
+        {
+            ExtractPercentage = 0;
+            ExtractedFilesCount = 0;
+            TotalFilesCount = -1;
+        }
+
+
+        [RelayCommand]
+        public void CancelWork()
+        {
+            shouldCancelWork = true;
+        }
+
         [RelayCommand]
         public async Task ExtractAsync()
         {
+            ResetExtractionStatistics();
             if (FileIndexes.Any())
             {
-                await extractor.ExtractFilesAsync(OutputDirPath, FileIndexes.ToArray());
+                isExtractingWholeArchive = false;
+                TotalFilesCount = FileIndexes.Distinct().Count();
+                await extractor.ExtractFilesAsync(OutputDirPath, FileIndexes.Distinct().ToArray());
             }
             else
             {
+                isExtractingWholeArchive = true;
                 await extractor.ExtractArchiveAsync(OutputDirPath);
             }
         }
