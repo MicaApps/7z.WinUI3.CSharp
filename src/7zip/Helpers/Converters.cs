3ad5d0c78b16b3df47ffb5b2dddbeb3e344d7c99
Jeff Zeng <zhh13950510580@outlook.com>
2023-10-01 20:58:19 +0800

    [chore] 资源迁移

--- src/7zip/Helpers/Converters.cs
+++ src/7zip/Helpers/Converters.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _7zip.ViewModels
+namespace _7zip.Helpers
 {
     public class PathToFileNameConverter : IValueConverter
     {
